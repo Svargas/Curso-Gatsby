c9490b180ea6d4577007203bd95632403ee8acb5
sergey <sergey@fpfTech.local>
2021-02-03 00:58:05 -0400

    primeiro commit

---
+++ src/pages/order.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+export default function OderPage() {
+  return (
+    <dv>
+      <p>Hey! I´m the Beers page next</p>
+    </dv>
+  );
+}
